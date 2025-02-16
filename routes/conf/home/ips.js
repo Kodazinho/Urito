13769a570b6d86dc2cf2528b2ab4c1fe583a31b3
Koda <andeyaelydio@gmail.com>
2025-02-16 11:25:48 -0300

    1.0.4

--- routes/conf/home/ips.js
+++ routes/conf/home/ips.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const controller = require('../../../controller/ipAccept');
+const fs = require("fs").promises;
+const path = require("path");
 const routes = express.Router();
 
-routes.get('/:action/:ip', async (req, res) => {
-    const { action, ip } = req.params;
-    
-})
+routes.get('/:action/:ip', controller);
 
 module.exports = routes;
